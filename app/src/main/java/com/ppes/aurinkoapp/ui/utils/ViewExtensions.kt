9f2890ad158cfbe280fae731a44d56c1b202a33f
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-09-02 18:01:07 +0300

    Add item layout and utils for view

---
+++ app/src/main/java/com/ppes/aurinkoapp/ui/utils/ViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.ppes.aurinkoapp.ui.utils
+
+import android.content.Context
+import android.view.View
+
+/**
+ * Created by ppes on 02/09/2017.
+ */
+
+val View.ctx: Context
+    get() = context
