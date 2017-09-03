b973638c67605ce89aaed607ca9361d11c3c470a
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-09-03 17:24:46 +0300

    Add customized delegate for App

--- app/src/main/java/com/ppes/aurinkoapp/ui/App.kt
+++ app/src/main/java/com/ppes/aurinkoapp/ui/App.kt
@@ -1,14 +1,14 @@
 package com.ppes.aurinkoapp.ui
 
 import android.app.Application
+import com.ppes.aurinkoapp.ui.utils.DelegatesExt
 
 /**
  * Created by ppes on 02/09/2017.
  */
 class App : Application() {
     companion object {
-        private var instance: Application? = null
-        fun instance() = instance!!
+        var instance: App by DelegatesExt.notNullSingleValue()
     }
 
     override fun onCreate() {
